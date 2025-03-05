0dd51a87fb900c3740c93dc235083f83e0aef322
Rodrigo Eduardo <eduardorodrigoalmeida18@gmail.com>
2025-03-04 23:00:18 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,35 @@
+// Função para adicionar o valor do botão clicado no display
+function input(value) {
+    document.getElementById('display').value += value;
+}
+
+// Função para calcular o valor do display
+function calculate() {
+    var display = document.getElementById('display');
+    display.value = eval(display.value);
+}
+
+// Função para limpar o display
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+// Adicionando funcionalidade para as teclas do teclado
+document.addEventListener('keydown', function (event) {
+    const key = event.key;
+    const display = document.getElementById('display');
+    // verificando se o valor da tecla é um número   
+    if (!isNaN(key)) {
+        display.value += key;
+    } else if (key === 'Enter') {
+        calculate();
+    } else if (key === 'Backspace') {
+        display.value = display.value.slice(0, -1);
+    } else if (key === 'Escape') {
+        clearDisplay();
+    } else if (key === '+' || key === '-' || key === '*' || key === '/') {
+        display.value += key;
+    } else if (key === '.') {
+        display.value += key;
+    }
+});
